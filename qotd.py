f4cb4162e565a2a310feddd714a8671f5f062297
Vraj Ved <vraj.a.ved@gmail.com>
2025-04-22 12:28:44 +0530

    Changed Variable name CYAN -> YELLOW

--- qotd.py
+++ qotd.py
@@ -2,9 +2,9 @@ import requests
 
 response = requests.get("https://zenquotes.io/api/random")
 data = response.json()
-CYAN  = "\033[33m"
+YELLOW  = "\033[33m"
 RESET = "\033[0m"
 
 qotd = data[0]['q'] + " - " + data[0]['a']
 
-print(CYAN + qotd + RESET)
+print(YELLOW + qotd + RESET)
